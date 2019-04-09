609ed5a999d43242cf929378ce9d07090f576cf0
Nick Doroshenko <doroshenkonk@gmail.com>
2019-04-09 11:38:31 +0300

    start forms template drive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'nglearn';
+    @ViewChild('form') viewChildForm: NgForm;
+  answers = [{
+    type: 'yes',
+    text: 'Да'
+  }, {
+    type: 'no',
+    text: 'Нет'
+  }];
+
+
+  submitForm(form: NgForm) {
+      console.log('sudmoit', form, this.viewChildForm);
+  }
 }
