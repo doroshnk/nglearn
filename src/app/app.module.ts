39595565c6aef2e5e8f206c2870e79135d685a71
Nick Doroshenko <doroshenkonk@gmail.com>
2019-04-10 21:57:20 +0300

    section 10 create shared module with common directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ColorDirective } from './shared/color.directive';
 import { HeaderComponent } from './header/header.component';
 import { CarsModule } from './cars-page/cars.module';
+import { SharedModule } from './shared/shared.module';
 
 
 
@@ -17,7 +17,6 @@ import { CarsModule } from './cars-page/cars.module';
   declarations: [
     AppComponent,
     HomePageComponent,
-    ColorDirective,
     HeaderComponent
   ],
   imports: [
@@ -25,7 +24,8 @@ import { CarsModule } from './cars-page/cars.module';
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    CarsModule
+    CarsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cars-page/cars.module.ts
+++ src/app/cars-page/cars.module.ts
@@ -5,11 +5,12 @@ import { CarsPageComponent } from './cars-page.component';
 import { CarPageComponent } from './car-page/car-page.component';
 import { CarsService } from '../cars.service';
 import { CarsRoutingModule } from './cars-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [CarsPageComponent, CarPageComponent],
     providers: [CarsService],
-    imports: [CommonModule, CarsRoutingModule]
+    imports: [CommonModule, CarsRoutingModule, SharedModule]
 })
 export class CarsModule {
 
