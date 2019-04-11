9f5e4b9bdabe92505e68e916ba1ef8ce812a2d89
Nick Doroshenko <doroshenkonk@gmail.com>
2019-04-11 08:09:54 +0300

    section 11 many complex animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,12 +25,36 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         //   transition('end => start', animate('800ms 500ms ease-out')),
           transition('start => active', animate(400)),
           transition('active => end', animate(400))
+      ]),
+      trigger('multi', [
+          state('start', style({
+              border: '5px solid black',
+              width: '150px',
+              height: '150px',
+          })),
+          state('end', style({
+              backgroundColor: 'blue',
+              width: '170px',
+              height: '170px',
+          })),
+           transition('start <=> end', [
+               style({backgroundColor: 'red'}),
+               animate('1500ms ease-in', style({
+                   backgroundColor: 'yellow'
+               })),
+               animate('1500ms ease-in', style({
+                   backgroundColor: 'green',
+                   width: '200px',
+                   height: '200px',
+               })),
+                animate(1000)
+           ]),
       ])
   ]
 })
 export class AppComponent {
   clickedDivState = 'start';
-
+  multiState = 'start';
   changeDivState() {
     this.clickedDivState = 'end';
     setTimeout(() => {
