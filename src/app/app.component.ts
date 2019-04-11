09df7f2f602fbfab86685f7ccaad8a81e75a42e7
Nick Doroshenko <doroshenkonk@gmail.com>
2019-04-11 07:21:11 +0300

    section 11 create animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  animations: [
+      trigger('clickedDiv', [
+          state('start', style({
+              backgroundColor: 'blue',
+              width: '150px',
+              height: '150px',
+          })),
+          state('end', style({
+              backgroundColor: 'red',
+              width: '300px',
+              height: '300px',
+          })),
+          transition('start => end', animate(1000)),
+          transition('end => start', animate('800ms 500ms ease-out'))
+      ])
+  ]
 })
 export class AppComponent {
-  title = 'nglearn';
+  clickedDivState = 'start';
+
+  changeDivState() {
+    this.clickedDivState = 'end';
+    setTimeout(() => {
+        this.clickedDivState = 'start';
+    }, 2000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
