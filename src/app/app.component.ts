8748bf050cee3234e8c69842a9d08ad7285e8921
Nick Doroshenko <doroshenkonk@gmail.com>
2019-04-30 02:39:13 +0300

    section 4 Renderer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  headerText = 'create directive';
+  headerText = 'create directive ';
 }
--- src/app/directives/bg.derictive.ts
+++ src/app/directives/bg.derictive.ts
@@ -1,14 +1,17 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 @Directive({
   selector: '[appBg]'
 })
 export class BgDirective implements OnInit {
-  constructor (private el: ElementRef){
+  constructor (private el: ElementRef, private renderer: Renderer2){
 
   }
 
   ngOnInit(){
     console.log(this.el)
-    this.el.nativeElement.style.backgroundColor = 'red';
+    const {nativeElement} = this.el;
+    this.renderer.setStyle(nativeElement,'background-color', 'blue');
+    this.renderer.addClass(nativeElement,'white-txt');
+    // this.el.nativeElement.style.backgroundColor = 'red';
   }
 }
