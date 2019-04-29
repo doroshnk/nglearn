e9e9a5f1527cee2fd40673956625b5bac8c1dcff
Nick Doroshenko <doroshenkonk@gmail.com>
2019-04-30 02:56:59 +0300

    section 4 directive events. HostListener HostBinding

--- src/app/directives/bg.derictive.ts
+++ src/app/directives/bg.derictive.ts
@@ -1,17 +1,32 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding} from '@angular/core';
 @Directive({
   selector: '[appBg]'
 })
 export class BgDirective implements OnInit {
+
+  @HostBinding('style.backgroundColor') BG : string;
+
   constructor (private el: ElementRef, private renderer: Renderer2){
 
   }
 
   ngOnInit(){
     console.log(this.el)
-    const {nativeElement} = this.el;
-    this.renderer.setStyle(nativeElement,'background-color', 'blue');
-    this.renderer.addClass(nativeElement,'white-txt');
+
     // this.el.nativeElement.style.backgroundColor = 'red';
   }
+
+  @HostListener('mouseenter', ['$event']) mouseEnter(event: Event){
+    const {nativeElement} = this.el;
+    this.BG = 'red';
+    this.renderer.addClass(nativeElement,'white-txt');
+    this.renderer.setStyle(nativeElement,'border', '1px solid black');
+  }
+  @HostListener('mouseleave') mouseLeave(){
+    const {nativeElement} = this.el;
+    this.BG = 'transparent';
+    this.renderer.setStyle(nativeElement,'border', 'none');
+    this.renderer.removeClass(nativeElement,'white-txt');
+  }
+
 }
